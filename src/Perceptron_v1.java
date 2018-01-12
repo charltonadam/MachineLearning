271c6997e76d98c0bda56bec4497bfcfb6685bac
Adam-Desktop <charltonadam@live.com>
2018-01-12 16:14:16 -0700

    early model of perceptron

--- src/Perceptron_v1.java
+++ src/Perceptron_v1.java
@@ -2,4 +2,62 @@ public class Perceptron_v1 {
 
     final double learningRate = .1;
 
+    final int theta = 1;
+    private double thetaWeight;
+
+    private int numberOfInputs;
+    private double[] weights;
+
+
+    public Perceptron_v1(int numberOfInputs) {
+        weights = new double[numberOfInputs + 1];
+        this.numberOfInputs = numberOfInputs;
+    }
+
+    public void learn(double[] inputs, boolean expected) {
+        if(inputs.length != numberOfInputs) {
+            System.out.println("Error, number of inputs does not match number of weights");
+            return;
+        }
+
+        if(test(inputs) != expected) {
+            //modify weights, as the test failed
+
+            //first, find out if we need to go up or down
+
+            int direction = -1;
+
+            if(expected) {
+                direction = 1;
+            }
+
+            for(int i = 0; i < numberOfInputs; i++) {
+                weights[i] += learningRate * direction * inputs[i];
+            }
+            thetaWeight += learningRate * direction * theta;
+
+
+        }
+
+
+
+
+    }
+
+    public boolean test(double[] inputs) {
+
+        double total = 0;
+        for(int i = 0; i < numberOfInputs; i++) {
+            total += inputs[i] * weights[i];
+        }
+        total += theta * thetaWeight;
+
+        if(total > 0) {
+            return true;
+        }
+        return false;
+    }
+
+
+
 }
