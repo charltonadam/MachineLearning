f5906881c700980db88392221ea88d6eeb40e370
Adam-Desktop <charltonadam@live.com>
2018-01-24 06:44:16 -0700

    started working on integrating the perceptron with the rest of the toolkit

---
+++ src/Perceptron.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class Perceptron extends SupervisedLearner {
+
+
+    public Perceptron(Random rand) {
+
+    }
+
+
+    @Override
+    public void train(Matrix features, Matrix labels) throws Exception {
+
+    }
+
+    @Override
+    public void predict(double[] features, double[] labels) throws Exception {
+
+    }
+
+
+
+
+
+    public static void main(String args[]) {
+
+        MLSystemManager runner = new MLSystemManager();
+
+
+    }
+
+}
